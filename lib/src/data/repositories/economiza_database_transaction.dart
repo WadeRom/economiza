ba14276f5821b8c7ff375304d52f257aefa2adee
Walter <wclrosromero@gmail.com>
2025-05-16 18:05:25 -0600

    refactor: Add EconomizaDatabaseTransaction wrapper for delegating to SqliteDatabaseTransaction

--- lib/src/data/repositories/economiza_database_transaction.dart
+++ lib/src/data/repositories/economiza_database_transaction.dart
@@ -1,19 +1,30 @@
+import 'package:economiza/src/data/database/economiza_database_manager.dart';
+import 'package:economiza/src/data/repositories/sqlite_database_transaction.dart';
 import 'package:economiza/src/interfaces/database/interface_database_transaction.dart';
 
-class EconomizaDatabaseTransaction {
-  final IDatabaseSimpleTransactionManager _databaseTransactionManager;
-  EconomizaDatabaseTransaction(this._databaseTransactionManager);
+class EconomizaDatabaseTransaction implements IDatabaseSimpleTransactionManager {
+  final Future<SqliteDatabaseTransaction> _sqliteTransactionFuture;
 
+  EconomizaDatabaseTransaction(EconomizaDatabaseManager manager)
+      : _sqliteTransactionFuture = manager.dbManager.getDatabase().then(
+            (db) => SqliteDatabaseTransaction(db),
+          );
+
+  @override
   Future<int> insert(SimpleInsertTransactionModel transaction) async {
-    return await _databaseTransactionManager.insert(transaction);
+    final sqliteTransaction = await _sqliteTransactionFuture;
+    return await sqliteTransaction.insert(transaction);
   }
 
+  @override
   Future<List<Map<String, dynamic>>> query(SimpleQueryTransactionModel transaction) async {
-    return await _databaseTransactionManager.query(transaction);
+    final sqliteTransaction = await _sqliteTransactionFuture;
+    return await sqliteTransaction.query(transaction);
   }
 
+  @override
   Future<int> update(SimpleUpdateTransactionModel transaction) async {
-    return await _databaseTransactionManager.update(transaction);
+    final sqliteTransaction = await _sqliteTransactionFuture;
+    return await sqliteTransaction.update(transaction);
   }
-
 }
