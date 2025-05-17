833c514714925272abe9db3474bcb13c71f73e27
walter <wclrosromero@gmail.com>
2025-05-17 12:50:05 -0600

    refactor(db): Mejor manejo de errores en EconomizaDatabaseTransaction
    - Se actualizaron los métodos ,  y  para devolver
    - Se añadió manejo de excepciones en cada operación para capturar errores correctamente
    - Ahora los errores generan un , evitando excepciones no manejadas
    - Mejora la estabilidad y control de errores en el manejo de transacciones

--- lib/src/data/repositories/economiza_database_transaction.dart
+++ lib/src/data/repositories/economiza_database_transaction.dart
@@ -1,6 +1,7 @@
 import 'package:economiza/src/data/database/economiza_database_manager.dart';
 import 'package:economiza/src/data/repositories/sqlite_database_transaction.dart';
 import 'package:economiza/src/interfaces/database/interface_database_transaction.dart';
+import 'package:economiza/src/interfaces/interface_uses_cases.dart';
 
 class EconomizaDatabaseTransaction implements IDatabaseSimpleTransactionManager {
   final Future<SqliteDatabaseTransaction> _sqliteTransactionFuture;
@@ -11,20 +12,32 @@ class EconomizaDatabaseTransaction implements IDatabaseSimpleTransactionManager 
           );
 
   @override
-  Future<int> insert(SimpleInsertTransactionModel transaction) async {
-    final sqliteTransaction = await _sqliteTransactionFuture;
-    return await sqliteTransaction.insert(transaction);
+  Future<UseCaseResult<int>> insert(SimpleInsertTransactionModel transaction) async {
+    try {
+      final sqliteTransaction = await _sqliteTransactionFuture;
+      return await sqliteTransaction.insert(transaction);
+    } catch (e) {
+      throw FailureUseCaseResult(message: "Error al insertar: ${e.toString()}");
+    }
   }
 
   @override
-  Future<List<Map<String, dynamic>>> query(SimpleQueryTransactionModel transaction) async {
-    final sqliteTransaction = await _sqliteTransactionFuture;
-    return await sqliteTransaction.query(transaction);
+  Future<UseCaseResult<List<Map<String, dynamic>>>> query(SimpleQueryTransactionModel transaction) async {
+    try {
+      final sqliteTransaction = await _sqliteTransactionFuture;
+      return await sqliteTransaction.query(transaction);
+    } catch (e) {
+      throw FailureUseCaseResult(message: "Error en la consulta: ${e.toString()}");
+    }
   }
 
   @override
-  Future<int> update(SimpleUpdateTransactionModel transaction) async {
-    final sqliteTransaction = await _sqliteTransactionFuture;
-    return await sqliteTransaction.update(transaction);
+  Future<UseCaseResult<int>> update(SimpleUpdateTransactionModel transaction) async {
+    try {
+      final sqliteTransaction = await _sqliteTransactionFuture;
+      return await sqliteTransaction.update(transaction);
+    } catch (e) {
+      throw FailureUseCaseResult(message: "Error al actualizar: ${e.toString()}");
+    }
   }
 }
